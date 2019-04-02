c02d68a8ebfe3db879727cce47cc435aa1b478a0
lukeludlow <lukeludlow@live.com>
2019-04-02 09:57:17 -0600

    actually search and filter by search text term

--- app/src/main/java/dev/lukel/familymap/ui/SearchFragment.java
+++ app/src/main/java/dev/lukel/familymap/ui/SearchFragment.java
@@ -5,10 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,8 +29,10 @@ import dev.lukel.familymap.model.Person;
 
 public class SearchFragment extends Fragment {
 
+    private final String TAG = "SEARCH_FRAGMENT";
     private RecyclerView searchRecyclerView;
     private SearchAdapter adapter;
+    private EditText searchText;
 
     public SearchFragment() {}
 
@@ -29,16 +41,35 @@ public class SearchFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "init search fragment");
         View v = inflater.inflate(R.layout.fragment_search, container, false);
         searchRecyclerView = (RecyclerView) v.findViewById(R.id.search_recycler_view);
         searchRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        searchText = (EditText) v.findViewById(R.id.search_edit_text);
+        searchText.addTextChangedListener(textWatcher);
+        updateUI();
         return v;
     }
 
-    private class SearchViewHolder extends RecyclerView.ViewHolder {
+    private class SearchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Person person;
+        private TextView titleText;
+        private TextView descriptionText;
         public SearchViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_search, parent, false));
+            itemView.setOnClickListener(this);
+            titleText = (TextView) itemView.findViewById(R.id.list_item_search_title);
+            descriptionText = (TextView) itemView.findViewById(R.id.list_item_search_details);
+        }
+        public void bind(Person p) {
+            person = p;
+            titleText.setText(p.getFirstName() + " " + p.getLastName());
+            descriptionText.setText(p.getPersonID());
+        }
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), person.getFirstName() + " clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -54,8 +85,9 @@ public class SearchFragment extends Fragment {
             return new SearchViewHolder(inflater, parent);
         }
         @Override
-        public void onBindViewHolder(@NonNull SearchViewHolder searchViewHolder, int i) {
-
+        public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
+            Person p = people.get(position);
+            holder.bind(p);
         }
         @Override
         public int getItemCount() {
@@ -64,9 +96,43 @@ public class SearchFragment extends Fragment {
     }
 
     private void updateUI() {
-        List<Person> people = Arrays.asList(DataSingleton.getPeople());
-        adapter = new SearchAdapter(people);
+        adapter = new SearchAdapter(searchPeople());
         searchRecyclerView.setAdapter(adapter);
     }
+
+    private List<Person> searchPeople() {
+        Log.i(TAG, "searching people...");
+        if (TextUtils.isEmpty(searchText.getText().toString())) {
+            Log.i(TAG, "search text empty, people = DataSingleton.getPeople()");
+            return Arrays.asList(DataSingleton.getPeople());
+        }
+        List<Person> found = new ArrayList<>();
+        String s = searchText.getText().toString().toLowerCase();
+        for (Person p : DataSingleton.getPeople()) {
+            if (p.getFirstName().toLowerCase().equals(s) || p.getLastName().toLowerCase().equals(s)) {
+                Log.i(TAG, "search found a match: " + p.getFirstName());
+                found.add(p);
+            }
+        }
+        return found;
+    }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //
+            Log.i(TAG, "onTextChanged");
+            updateUI();
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.i(TAG, "afterTextChanged: " + s.toString());
+            updateUI();
+        }
+    };
 
 }
